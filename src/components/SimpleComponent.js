e349e123e1ecf492b799be08cdf5505d82997574
Your Name <as19@williams.edu>
2019-09-23 21:20:00 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,21 @@
 // Code SimpleComponent Here
+import React, {Component} from 'react'
+
+export default class SimpleComponent extends Component {
+
+  state = {
+    mood: "happy"
+  }
+
+  switch = () => {
+    const mood = (this.state.mood==="happy") ? "sad" : "happy"
+    this.setState({
+      mood: mood
+    })
+  }
+
+
+  render(){
+    return <div onClick={this.switch}>{this.state.mood}</div>
+  }
+}
